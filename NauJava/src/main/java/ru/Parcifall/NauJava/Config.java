806bee954c86994941d9f9b06c48d29a8c61f3eb
Parcifall <i.chuxarev205@yandex.ru>
2024-10-07 20:59:26 +0500

    Реализация бина для имитации работы БД

---
+++ NauJava/src/main/java/ru/Parcifall/NauJava/Config.java
@@ -0,0 +1,18 @@
+package ru.Parcifall.NauJava;
+
+import Entitys.Task;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.ArrayList;
+
+@Configuration
+public class Config {
+    @Bean
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    public ArrayList<Task> taskContainer() {
+        return new ArrayList<>();
+    }
+}

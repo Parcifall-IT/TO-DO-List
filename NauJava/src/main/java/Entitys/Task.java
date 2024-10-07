9c518b6c6a738a4aeeada204eb484e72547f19fc
Parcifall <i.chuxarev205@yandex.ru>
2024-10-07 19:58:13 +0500

    Реализация класса сущности БД

---
+++ NauJava/src/main/java/Entitys/Task.java
@@ -0,0 +1,70 @@
+package Entitys;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/*
+Управление задачами (To-Do List). Можно заводить задачи, менять статусы,
+устанавливать сроки выполнения, получать оповещения.
+ */
+public class Task {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Calendar deadline;
+
+    private Status status;
+
+    public Task(Long id, String name, String description, Calendar deadline, Status status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.deadline = deadline;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Calendar getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Calendar deadline) {
+        this.deadline = deadline;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
+
+enum Status { FAILED, INITIALISED, FORGOTTEN, PROGRESS, FINISHED }
